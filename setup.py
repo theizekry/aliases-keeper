46ec9e8e6f4516438cdcdb4251ab7cae621dcc1a
Islam Zekry <theizekry@gmail.com>
2024-05-18 11:17:33 +0300

    Aliases keeper - added bin for macos

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ import shutil
 import os
 
 class GenerateCommand(Command):
-    """A custom command to generate the executable using PyInstaller."""
+    """A custom command to generate the executables using PyInstaller."""
 
-    description = 'Generate a single executable file using PyInstaller.'
+    description = 'Generate single executable files using PyInstaller.'
     user_options = []
 
     def initialize_options(self):
@@ -16,8 +16,17 @@ class GenerateCommand(Command):
         pass
 
     def run(self):
-        # Run PyInstaller command
-        subprocess.call(['pyinstaller', '--onefile', '--distpath', './bin', './src/aliasify.py'])
+        # Run PyInstaller command for Linux
+        subprocess.call(['pyinstaller', '--onefile', '--distpath', './bin/linux', './src/aliasify.py'])
+
+        # Remove the build directory
+        shutil.rmtree('build', ignore_errors=True)
+
+        # Remove the spec file
+        os.remove('aliasify.spec')
+
+        # Run PyInstaller command for macOS
+        subprocess.call(['pyinstaller', '--onefile', '--distpath', './bin/macos', './src/aliasify.py'])
 
         # Remove the build directory
         shutil.rmtree('build', ignore_errors=True)

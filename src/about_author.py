560adf851c8e97c469ebe0cf4849361dba38e6c4
Islam Zekry <theizekry@gmail.com>
2024-05-20 13:03:41 +0300

    added aliasify to about author

--- src/about_author.py
+++ src/about_author.py
@@ -1,5 +1,11 @@
 def about_author():
-    about = """\
+    about = """
+    _    _  _              _   __        
+   /_\  | |(_)  __ _  ___ (_) / _| _   _ 
+  //_\\ | || | / _` |/ __|| || |_ | | | |
+ /  _  \| || || (_| |\__ \| ||  _|| |_| |
+ \_/ \_/|_||_| \__,_||___/|_||_|   \__, |
+
 # Unix Aliases Keeper - Manage and synchronize your aliases.
 # Data 17/05/2024.
 # Developed by Islam Zekry https://github.com/theizekry/aliases-keeper.git.

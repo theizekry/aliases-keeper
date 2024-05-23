d27bc4016dacd2866551aa06c021fc9307af92cc
Islam Zekry <theizekry@gmail.com>
2024-05-24 01:04:41 +0300

    Finished flush command

--- src/aliasify.py
+++ src/aliasify.py
@@ -1,6 +1,6 @@
 import click
 from colorama import Fore, Style
-from crud import create_alias, delete_alias, show_aliases, edit_aliases
+from crud import create_alias, delete_alias, show_aliases, edit_aliases, source_shell_profile, flush_aliases
 from about_author import about_author
 
 @click.group()
@@ -23,6 +23,7 @@ def about():
 def create(alias_name, alias_value, group):
     """Create a new alias"""
     create_alias(alias_name, alias_value, group)
+    source_shell_profile()
 
 
 @main.command(short_help="Create a new alias")
@@ -30,6 +31,7 @@ def create(alias_name, alias_value, group):
 def delete(alias_name):
     """Delete an alias by name"""
     delete_alias(alias_name)
+    source_shell_profile()
 
 
 @main.command(short_help="Edit an alias by name, you can edit name and value along as the group of the alias")
@@ -39,12 +41,22 @@ def delete(alias_name):
 def edit(alias_name, alias_value, group):
     """Edit an alias by name"""
     edit_aliases(alias_name, alias_value, group)
+    source_shell_profile()
 
 
 @main.command(short_help="Show aliases File content")
 def show():
     """Show the Current Aliases"""
     show_aliases()
+    source_shell_profile()
+
+
+@main.command(short_help="Flush all the aliases.")
+def flush():
+    """Show the Current Aliases"""
+    if click.confirm('All the saved aliases will be flushed. Do you want to continue?', abort=True):
+        flush_aliases()
+        source_shell_profile()
 
 
 if __name__ == "__main__":
--- src/crud.py
+++ src/crud.py
@@ -2,20 +2,34 @@ import click
 import colorama
 import os
 import subprocess
-from colorama import Fore
-
+from colorama import Fore, init
 from about_author import about_author
 
 # Initialize colorama
 colorama.init()
+init(autoreset=True)
 
-# Directory to store aliases file
-ALIASES_DIR = os.path.expanduser("~/.aliases_keeper")
-ALIASES_FILE = os.path.join(ALIASES_DIR, "aliases.txt")
-LOG_OUTPUT = True
+# Directory to store aliases files
+ALIASES_DIR   = os.path.expanduser("~/.aliases_keeper")
+ALIASES_FILE  = os.path.join(ALIASES_DIR, "aliasify")
+PROFILE_FILES = [os.path.expanduser("~/.bashrc"), os.path.expanduser("~/.zshrc")]
 
+LOG_OUTPUT    = True
 
-def ensure_aliases_file_exists():
+# Mapped shells to their profile files path.
+SHELL_PROFILE_MAP = {
+    "bash": "~/.bashrc",
+    "zsh": "~/.zshrc",
+    "ksh": "~/.kshrc",
+    "csh": "~/.cshrc",
+    "tcsh": "~/.tcshrc",
+    "fish": "~/.config/fish/config.fish",
+    "ash": "~/.profile",
+    "dash": "~/.profile",
+    "sh": "~/.profile",
+}
+
+def ensure_aliases_file_exists_and_sourced():
     """Ensure the aliases file exists, creating it if necessary"""
     """Create the aliases file with initial content"""
 
@@ -25,6 +39,54 @@ def ensure_aliases_file_exists():
     if not os.path.exists(ALIASES_FILE):
         with open(ALIASES_FILE, "w") as f:
             f.write(f"{about_author()}\n")
+
+    alias_line = f"source \"{ALIASES_FILE}\""
+
+    profile_file = detect_active_shell_profile()
+
+    if not profile_file:
+        click.echo(Fore.RED + "Could not detect active shell or unsupported shell.")
+
+    if os.path.exists(profile_file):
+        with open(profile_file, "r") as file:
+            content = file.read()
+
+    if alias_line not in content:
+        with open(profile_file, "a") as file:
+            file.write(f"\n# Aliases Keeper\n{alias_line}\n")
+            click.echo(Fore.YELLOW + f"Added {ALIASES_FILE} to {profile_file}")
+
+
+def source_shell_profile():
+    """Source the Shell profile environment to make new aliases available"""
+    # profile_file = detect_active_shell_profile()
+    # command = f"exec {profile_file}"
+    #
+    # if not profile_file:
+    #     click.echo(Fore.RED + "Could not detect active shell or unsupported shell.")
+    #     return;
+    #
+    # command = f". {profile_file}"
+    # #process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    # subprocess.run(f'. {profile_file}', shell=True)
+    # # stdout, stderr = process.communicate()
+    # #
+    # # if process.returncode == 0:
+    # #     click.echo(Fore.GREEN + f"Sourced {profile_file} to refresh the shell environment.")
+    # # else:
+    # #     click.echo(Fore.RED + f"Failed to source {profile_file} - Error: {stderr}")
+    # #     click.echo(Fore.RED + "Please open a new terminal session or source the file manually.")
+
+
+def detect_active_shell_profile():
+    """Detect the active shell and return the corresponding profile file"""
+    shell = os.path.basename(os.environ.get("SHELL", ""))
+
+    profile = SHELL_PROFILE_MAP.get(shell)
+
+    if profile:
+        return os.path.expanduser(profile)
+    return None
 
 
 def alias_exists(alias_name):
@@ -39,7 +101,7 @@ def alias_exists(alias_name):
 
 def create_alias(alias_name, alias_value, group="Other"):
     """Create a new alias under the specified group"""
-    ensure_aliases_file_exists()
+    ensure_aliases_file_exists_and_sourced()
 
     with open(ALIASES_FILE, "r") as f:
         lines = f.readlines()
@@ -69,7 +131,7 @@ def create_alias(alias_name, alias_value, group="Other"):
 def delete_alias(alias_name):
     """Delete an alias from the aliases file by name"""
 
-    ensure_aliases_file_exists()
+    ensure_aliases_file_exists_and_sourced()
 
     if not alias_exists(alias_name):
         click.echo(Fore.RED + f"Alias '{alias_name}' does not exist.")
@@ -97,7 +159,7 @@ def delete_alias(alias_name):
 def edit_aliases(alias_name, alias_value, group):
     global LOG_OUTPUT  # modify the global LOG_OUTPUT variable
 
-    ensure_aliases_file_exists()
+    ensure_aliases_file_exists_and_sourced()
 
     LOG_OUTPUT = False
 
@@ -111,8 +173,13 @@ def edit_aliases(alias_name, alias_value, group):
 
 
 def show_aliases():
-    ensure_aliases_file_exists()
-
+    ensure_aliases_file_exists_and_sourced()
     output = subprocess.check_output(["cat", ALIASES_FILE], universal_newlines=True)
-
     print(output)
+
+
+def flush_aliases():
+    ensure_aliases_file_exists_and_sourced()
+    with open(ALIASES_FILE, 'w'):
+        pass
+
